96886d68db1cfcc400c6611ce9c237ce670ee91e
alex <alexander.harding@gmail.com>
2024-10-12 11:44:28 -0400

    init commit

---
+++ init.py
@@ -0,0 +1,2 @@
+print("init")
+import nuke_harding_python as nhp
---
+++ menu.py
@@ -0,0 +1,4 @@
+from NukeTools.nukeserversocket import nukeserversocket
+nukeserversocket.install_nuke()
+from NukeTools.vimdcc import vimdcc
+vimdcc.install_nuke()
